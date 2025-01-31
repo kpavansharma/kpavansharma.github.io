dddef86c01e25cf14ca6d19450a1788f589e56c1
kpavansharma <82900099+kpavansharma@users.noreply.github.com>
2025-01-31 11:46:49 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Qlucent</h2>
+            <p>Qlucent is dedicated to elevating your digital security through innovative IT solutions.</p>
+        </div>
+    );
+};
+
+export default About;
